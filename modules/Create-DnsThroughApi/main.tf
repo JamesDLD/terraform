e6d34e8046d239f2b44df9c25f98758d0e4119aa
UniWare <j.dumont@veebaze.com>
2018-04-23 13:46:43 +0200

    Using json template for VM Backup enabling & move bash file to module repo

--- modules/Create-DnsThroughApi/main.tf
+++ modules/Create-DnsThroughApi/main.tf
@@ -10,12 +10,12 @@ resource "null_resource" "vm_dns_records" {
   }
 
   provisioner "local-exec" {
-    command = "bash ./bash/Create-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.vm_prefix}${lookup(var.Vms[count.index], "suffix_name")}${lookup(var.Vms[count.index], "id")} ${lookup(var.Vms[count.index], "static_ip")}"
+    command = "bash ./modules/Create-DnsThroughApi/Create-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.vm_prefix}${lookup(var.Vms[count.index], "suffix_name")}${lookup(var.Vms[count.index], "id")} ${lookup(var.Vms[count.index], "static_ip")}"
   }
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "bash ./bash/Delete-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.vm_prefix}${lookup(var.Vms[count.index], "suffix_name")}${lookup(var.Vms[count.index], "id")} ${lookup(var.Vms[count.index], "static_ip")}"
+    command = "bash ./modules/Create-DnsThroughApi/Delete-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.vm_prefix}${lookup(var.Vms[count.index], "suffix_name")}${lookup(var.Vms[count.index], "id")} ${lookup(var.Vms[count.index], "static_ip")}"
   }
 }
 
@@ -31,12 +31,12 @@ resource "null_resource" "lb_dns_records" {
   }
 
   provisioner "local-exec" {
-    command = "bash ./bash/Create-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.lb_prefix}${lookup(var.Lbs[count.index], "suffix_name")} ${lookup(var.Lbs[count.index], "static_ip")}"
+    command = "bash ./modules/Create-DnsThroughApi/Create-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.lb_prefix}${lookup(var.Lbs[count.index], "suffix_name")} ${lookup(var.Lbs[count.index], "static_ip")}"
   }
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "bash ./bash/Delete-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.lb_prefix}${lookup(var.Lbs[count.index], "suffix_name")} ${lookup(var.Lbs[count.index], "static_ip")}"
+    command = "bash ./modules/Create-DnsThroughApi/Delete-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${var.xpod_dns_zone_name} ${var.lb_prefix}${lookup(var.Lbs[count.index], "suffix_name")} ${lookup(var.Lbs[count.index], "static_ip")}"
   }
 }
 
@@ -52,11 +52,11 @@ resource "null_resource" "dns_records_publication" {
   }
 
   provisioner "local-exec" {
-    command = "bash ./bash/Publish-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${lookup(var.Dns_Wan_Records[count.index], "hostname")} ${lookup(var.Dns_Wan_Records[count.index], "static_ip")}"
+    command = "bash ./modules/Create-DnsThroughApi/Publish-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${lookup(var.Dns_Wan_Records[count.index], "hostname")} ${lookup(var.Dns_Wan_Records[count.index], "static_ip")}"
   }
 
   provisioner "local-exec" {
     when    = "destroy"
-    command = "bash ./bash/Unpublish-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${lookup(var.Dns_Wan_Records[count.index], "hostname")} ${lookup(var.Dns_Wan_Records[count.index], "static_ip")}"
+    command = "bash ./modules/Create-DnsThroughApi/Unpublish-Dns.sh ${var.dns_fqdn_api} ${var.dns_secret} ${var.dns_application_name} ${lookup(var.Dns_Wan_Records[count.index], "hostname")} ${lookup(var.Dns_Wan_Records[count.index], "static_ip")}"
   }
 }
