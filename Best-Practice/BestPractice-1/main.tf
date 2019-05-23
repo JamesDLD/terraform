c318829c1ae3f3d3f3f253069b9ebc3354c1f108
James Dumont <j.dumont@veebaze.com>
2019-05-23 18:24:58 +0200

    BP1 terraform 0.12upgrade

--- Best-Practice/BestPractice-1/main.tf
+++ Best-Practice/BestPractice-1/main.tf
@@ -10,10 +10,10 @@ terraform {
 
 #Set the Provider
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
-  tenant_id       = "${var.tenant_id}"
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 #Call module/resource
@@ -33,12 +33,15 @@ resource "azurerm_virtual_network" "bp1-vnet1" {
   }
 }
 
-data "azurerm_subscription" "primary" {}
+data "azurerm_subscription" "primary" {
+}
 
-data "azurerm_client_config" "test" {}
+data "azurerm_client_config" "test" {
+}
 
 resource "azurerm_role_assignment" "test" {
   scope                = azurerm_virtual_network.bp1-vnet1.id
   role_definition_name = "Reader"
   principal_id         = data.azurerm_client_config.test.service_principal_object_id
 }
+
