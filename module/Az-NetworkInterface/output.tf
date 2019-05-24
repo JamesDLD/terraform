c3f774144d246b7ce5468c4bbfc71fc1d8d59aad
James Dumont <j.dumont@veebaze.com>
2019-05-24 19:39:47 +0200

    Az-NetworkInterface ok with terraform 0.12

--- module/Az-NetworkInterface/output.tf
+++ module/Az-NetworkInterface/output.tf
@@ -1,23 +1,24 @@
 output "Linux_nics_names" {
-  value = "${azurerm_network_interface.linux_vms_nics.*.name}"
+  value = azurerm_network_interface.linux_vms_nics.*.name
 }
 
 output "Linux_nics_ids" {
-  value = "${azurerm_network_interface.linux_vms_nics.*.id}"
+  value = azurerm_network_interface.linux_vms_nics.*.id
 }
 
 output "Linux_nics_private_ip_address" {
-  value = "${azurerm_network_interface.linux_vms_nics.*.private_ip_address}"
+  value = azurerm_network_interface.linux_vms_nics.*.private_ip_address
 }
 
 output "Windows_nics_names" {
-  value = "${azurerm_network_interface.Windows_Vms_nics.*.name}"
+  value = azurerm_network_interface.Windows_Vms_nics.*.name
 }
 
 output "Windows_nics_ids" {
-  value = "${azurerm_network_interface.Windows_Vms_nics.*.id}"
+  value = azurerm_network_interface.Windows_Vms_nics.*.id
 }
 
 output "Windows_nics_private_ip_address" {
-  value = "${azurerm_network_interface.Windows_Vms_nics.*.private_ip_address}"
+  value = azurerm_network_interface.Windows_Vms_nics.*.private_ip_address
 }
+
--- module/Az-NetworkInterface/var.tf
+++ module/Az-NetworkInterface/var.tf
@@ -1,43 +1,83 @@
-provider "null" {
-  version = "2.1.2"
+variable "subscription_id" {
+  description="Azure subscription Id."
 }
 
-variable "subscription_id" {}
-
 variable "Linux_Vms" {
-  type = "list"
+  description="Linux VMs list."
+  type = list(object({
+    suffix_name                   = string
+    id                            = number               #Id of the VM
+    Id_Lb                         = number               #Id of the Load Balancer, set to 777 if there is no Load Balancer
+    Id_Lb_Public                  = number             #Id of the public Load Balancer, set to 777 if there is no public Load Balancer
+    Id_Ip_Public                  = number             #Id of the public Ip, set to 777 if there is no public Ip
+    Id_Subnet                     = number               #Id of the Subnet
+    zone                          = number               #Availability Zone id, could be 1, 2 or 3, if you don't need to set it to "", WARNING you could not have Availabilitysets and AvailabilityZones
+    Id_Nsg                        = number             #Id of the Network Security Group, set to 777 if there is no Network Security Groups
+    BackupPolicyName              = string             #Set 777 to disable backup (WARNING, this will delete previous backup) otherwise set a backup policy like BackupPolicy-Schedule1
+    static_ip                     = string        #Set 777 to get dynamic IP
+    vm_size                       = string
+    managed_disk_type             = string
+    enable_accelerated_networking = bool
+  }))
 }
 
 variable "Windows_Vms" {
-  type = "list"
+  description="Linux VMs list."
+  type = list(object({
+    suffix_name                   = string
+    id                            = number               #Id of the VM
+    Id_Lb                         = number               #Id of the Load Balancer, set to 777 if there is no Load Balancer
+    Id_Lb_Public                  = number             #Id of the public Load Balancer, set to 777 if there is no public Load Balancer
+    Id_Ip_Public                  = number             #Id of the public Ip, set to 777 if there is no public Ip
+    Id_Subnet                     = number               #Id of the Subnet
+    zone                          = number               #Availability Zone id, could be 1, 2 or 3, if you don't need to set it to "", WARNING you could not have Availabilitysets and AvailabilityZones
+    Id_Nsg                        = number             #Id of the Network Security Group, set to 777 if there is no Network Security Groups
+    BackupPolicyName              = string             #Set 777 to disable backup (WARNING, this will delete previous backup) otherwise set a backup policy like BackupPolicy-Schedule1
+    static_ip                     = string        #Set 777 to get dynamic IP
+    vm_size                       = string
+    managed_disk_type             = string
+    enable_accelerated_networking = bool
+  }))
 }
 
-variable "nic_suffix" {}
-variable "nic_prefix" {}
-variable "nic_location" {}
-variable "nic_resource_group_name" {}
+variable "nic_suffix" {
+  description="Network Interfaces suffix."
+}
+
+variable "nic_prefix" {
+  description="Network Interfaces prefix."
+}
+
+variable "nic_location" {
+  description="Network Interfaces location."
+}
+
+variable "nic_resource_group_name" {
+  description="Network Interfaces resource group name."
+}
 
 variable "subnets_ids" {
-  type = "list"
+  description="Network Interfaces subnets list."
+  type = list(string)
 }
 
 variable "lb_backend_ids" {
-  type = "list"
+  description="Network Interfaces internal load balancers backend ids list."
+  type = list(string)
 }
 
 variable "lb_backend_Public_ids" {
-  type = "list"
+  description="Network Interfaces public load balancers backend ids list."
+  type = list(string)
 }
 
 variable "nic_tags" {
-  type = "map"
+  description="Network Interfaces tags."
+  type = map(string)
 }
 
 variable "nsgs_ids" {
-  type = "list"
+  description="Network Interfaces network security ids."
+  type = list(string)
 }
 
-variable "use_old_ip_configuration_name" {
-  default     = "false"
-  description = "Only used by the vpod team for retro compatibility with old model"
-}
