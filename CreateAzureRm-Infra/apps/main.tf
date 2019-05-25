efff1ce22c5c3497c2491948e92c2d071f71df8e
James Dumont <j.dumont@veebaze.com>
2019-05-25 18:31:49 +0200

    Module Create-AzureRmRecoveryServicesVault ok for Terraform 0.12

--- CreateAzureRm-Infra/apps/main.tf
+++ CreateAzureRm-Infra/apps/main.tf
@@ -54,7 +54,7 @@ module "Az-NetworkSecurityGroup-Apps" {
   nsgrules                = var.apps_nsgrules
 }
 
-module "Create-AzureRmSubnet-Apps" {
+module "Az-Subnet-Apps" {
   source                     = "github.com/JamesDLD/terraform/module/Az-Subnet"
   subscription_id            = var.subscription_id
   subnet_resource_group_name = var.rg_infr_name
@@ -66,7 +66,7 @@ module "Create-AzureRmSubnet-Apps" {
 
 ## Virtual Machines components
 
-module "Create-AzureRmLoadBalancer-Apps" {
+module "Az-LoadBalancer-Apps" {
   source                 = "github.com/JamesDLD/terraform/module/Az-LoadBalancer"
   Lbs                    = var.Lbs
   lb_prefix              = "${var.app_name}-${var.env_name}-"
@@ -74,12 +74,12 @@ module "Create-AzureRmLoadBalancer-Apps" {
   lb_location            = data.azurerm_resource_group.MyApps.location
   lb_resource_group_name = data.azurerm_resource_group.MyApps.name
   Lb_sku                 = var.Lb_sku
-  subnets_ids            = module.Create-AzureRmSubnet-Apps.subnets_ids
+  subnets_ids            = module.Az-Subnet-Apps.subnets_ids
   lb_tags                = data.azurerm_resource_group.MyApps.tags
   LbRules                = var.LbRules
 }
 
-module "Create-AzureRmNetworkInterface-Apps" {
+module "Az-NetworkInterface-Apps" {
   source                  = "github.com/JamesDLD/terraform/module/Az-NetworkInterface"
   subscription_id         = var.subscription_id
   Linux_Vms               = var.Linux_Vms   #If no need just fill "Linux_Vms = []" in the tfvars file
@@ -88,14 +88,14 @@ module "Create-AzureRmNetworkInterface-Apps" {
   nic_suffix              = "-nic1"
   nic_location            = data.azurerm_resource_group.MyApps.location
   nic_resource_group_name = data.azurerm_resource_group.MyApps.name
-  subnets_ids             = module.Create-AzureRmSubnet-Apps.subnets_ids
-  lb_backend_ids          = module.Create-AzureRmLoadBalancer-Apps.lb_backend_ids
+  subnets_ids             = module.Az-Subnet-Apps.subnets_ids
+  lb_backend_ids          = module.Az-LoadBalancer-Apps.lb_backend_ids
   lb_backend_Public_ids   = ["null"]
   nic_tags                = data.azurerm_resource_group.MyApps.tags
   nsgs_ids                = module.Az-NetworkSecurityGroup-Apps.nsgs_ids
 }
 
-module "Create-AzureRmVm-Apps" {
+module "Az-Vm-Apps" {
   source                             = "github.com/JamesDLD/terraform/module/Az-Vm"
   subscription_id                    = var.subscription_id
   sa_bootdiag_storage_uri            = data.azurerm_storage_account.Infr.primary_blob_endpoint
@@ -105,13 +105,13 @@ module "Create-AzureRmVm-Apps" {
   workspace_name                     = ""
 
   Linux_Vms                     = var.Linux_Vms #If no need just fill "Linux_Vms = []" in the tfvars file
-  Linux_nics_ids                = module.Create-AzureRmNetworkInterface-Apps.Linux_nics_ids
+  Linux_nics_ids                = module.Az-NetworkInterface-Apps.Linux_nics_ids
   Linux_storage_image_reference = var.Linux_storage_image_reference
   Linux_DataDisks               = var.Linux_DataDisks
   ssh_key                       = var.ssh_key
 
   Windows_Vms                     = var.Windows_Vms #If no need just fill "Windows_Vms = []" in the tfvars file
-  Windows_nics_ids                = module.Create-AzureRmNetworkInterface-Apps.Windows_nics_ids
+  Windows_nics_ids                = module.Az-NetworkInterface-Apps.Windows_nics_ids
   Windows_storage_image_reference = var.Windows_storage_image_reference #If no need just fill "Windows_storage_image_reference = []" in the tfvars file
   Windows_DataDisks               = var.Windows_DataDisks
 
@@ -125,8 +125,8 @@ module "Create-AzureRmVm-Apps" {
 
 /*
 #Need improvment 1 : NEED MAJOR UPDATE to fit with AzureRM provider 1.21.0
-module "Create-AzureRmVmss-Apps" {
-  source                   = "github.com/JamesDLD/terraform/module/Create-AzureRmVmss"
+module "Az-Vmss-Apps" {
+  source                   = "github.com/JamesDLD/terraform/module/Az-Vmss"
   sa_bootdiag_storage_uri  = "${data.azurerm_storage_account.Infr.primary_blob_endpoint}"
   Linux_Ss_Vms             = ["${var.Linux_Ss_Vms}"]                                                #If no need just fill "Linux_Vms = []" in the tfvars file
   Windows_Ss_Vms           = ["${var.Windows_Ss_Vms}"]                                              #If no need just fill "Linux_Vms = []" in the tfvars file
@@ -137,23 +137,23 @@ module "Create-AzureRmVmss-Apps" {
   app_admin                = "${var.app_admin}"
   pass                     = "${var.pass}"
   ssh_key                  = "${var.ssh_key}"
-  subnets_ids              = "${module.Create-AzureRmSubnet-Apps.subnets_ids}"
-  lb_backend_ids           = "${module.Create-AzureRmLoadBalancer-Apps.lb_backend_ids}"
+  subnets_ids              = "${module.Az-Subnet-Apps.subnets_ids}"
+  lb_backend_ids           = "${module.Az-LoadBalancer-Apps.lb_backend_ids}"
   nsgs_ids                 = "${module.Az-NetworkSecurityGroup-Apps.nsgs_ids}"
 }
 */
 
 # Infra cross services for Apps
-module "Enable-AzureRmRecoveryServicesBackupProtection-Apps" {
+module "Az-RecoveryServicesBackupProtection-Apps" {
   source          = "github.com/JamesDLD/terraform/module/Az-RecoveryServicesBackupProtection"
   subscription_id = var.subscription_id
   bck_vms_names = concat(
-    module.Create-AzureRmVm-Apps.Linux_Vms_names,
-    module.Create-AzureRmVm-Apps.Windows_Vms_names,
+    module.Az-Vm-Apps.Linux_Vms_names,
+    module.Az-Vm-Apps.Windows_Vms_names,
   ) #Names of the resources to backup
   bck_vms_resource_group_names = concat(
-    module.Create-AzureRmVm-Apps.Linux_Vms_rgnames,
-    module.Create-AzureRmVm-Apps.Windows_Vms_rgnames,
+    module.Az-Vm-Apps.Linux_Vms_rgnames,
+    module.Az-Vm-Apps.Windows_Vms_rgnames,
   ) #Resource Group Names of the resources to backup
   bck_vms                     = concat(var.Linux_Vms, var.Windows_Vms)
   bck_rsv_name                = var.bck_rsv_name
@@ -161,13 +161,5 @@ module "Enable-AzureRmRecoveryServicesBackupProtection-Apps" {
 }
 
 ## Infra common services
-module "Create-AzureRmAutomationAccount-Apps" {
-  source                   = "github.com/JamesDLD/terraform/module/Create-AzureRmAutomationAccount"
-  auto_name                = "${var.app_name}-${var.env_name}-auto1"
-  auto_location            = data.azurerm_resource_group.MyApps.location
-  auto_resource_group_name = data.azurerm_resource_group.MyApps.name
-  auto_sku                 = var.auto_sku
-  auto_tags                = data.azurerm_resource_group.MyApps.tags
-  auto_credentials         = var.service_principals #If no need just set to []
-}
+#N/A
 
--- module/Create-AzureRmRecoveryServicesVault/main.tf
+++ module/Create-AzureRmRecoveryServicesVault/main.tf
@@ -1,33 +1,34 @@
 resource "azurerm_template_deployment" "backup_vault" {
   name                = "${var.rsv_name}-DEP"
-  resource_group_name = "${var.rsv_resource_group_name}"
-  template_body       = "${file("${path.module}/AzureRmRecoveryServicesVault_template.json")}"
+  resource_group_name = var.rsv_resource_group_name
+  template_body       = file("${path.module}/AzureRmRecoveryServicesVault_template.json")
   deployment_mode     = "Incremental"
 
   parameters = {
-    vaultName = "${var.rsv_name}"
-    rsvtags   = "${jsonencode(var.rsv_tags)}"
+    vaultName = var.rsv_name
+    rsvtags   = jsonencode(var.rsv_tags)
   }
-
   #Terraform sends only string to azurerm template, I had to convert it to integer or array into the ARM Template Json, for info : https://github.com/terraform-providers/terraform-provider-azurerm/issues/34
 }
 
 resource "azurerm_template_deployment" "backup_policies" {
-  count               = "${length(var.rsv_backup_policies)}"
-  name                = "${azurerm_template_deployment.backup_vault.outputs["vaultName"]}-${lookup(var.rsv_backup_policies[count.index], "Name")}-DEP"
-  resource_group_name = "${var.rsv_resource_group_name}"
-  template_body       = "${file("${path.module}/AzureRmRecoveryServicesVaultPolicy_template.json")}"
-  deployment_mode     = "Incremental"
+  count               = length(var.rsv_backup_policies)
+  name                = "${azurerm_template_deployment.backup_vault.outputs["vaultName"]}-${var.rsv_backup_policies[count.index]["Name"]}-DEP"
+  resource_group_name = var.rsv_resource_group_name
+  template_body = file(
+    "${path.module}/AzureRmRecoveryServicesVaultPolicy_template.json",
+  )
+  deployment_mode = "Incremental"
 
   parameters = {
-    vaultName                                    = "${azurerm_template_deployment.backup_vault.outputs["vaultName"]}"
-    policyName                                   = "${lookup(var.rsv_backup_policies[count.index], "Name")}"
-    StringToConvertscheduleRunTimes              = "${lookup(var.rsv_backup_policies[count.index], "scheduleRunTimes")}"
-    timeZone                                     = "${lookup(var.rsv_backup_policies[count.index], "timeZone")}"
-    StringToConvertdailyRetentionDurationCount   = "${lookup(var.rsv_backup_policies[count.index], "dailyRetentionDurationCount")}"
-    StringToConvertweeklyRetentionDurationCount  = "${lookup(var.rsv_backup_policies[count.index], "weeklyRetentionDurationCount")}"
-    StringToConvertmonthlyRetentionDurationCount = "${lookup(var.rsv_backup_policies[count.index], "monthlyRetentionDurationCount")}"
+    vaultName                                    = azurerm_template_deployment.backup_vault.outputs["vaultName"]
+    policyName                                   = var.rsv_backup_policies[count.index]["Name"]
+    StringToConvertscheduleRunTimes              = var.rsv_backup_policies[count.index]["scheduleRunTimes"]
+    timeZone                                     = var.rsv_backup_policies[count.index]["timeZone"]
+    StringToConvertdailyRetentionDurationCount   = var.rsv_backup_policies[count.index]["dailyRetentionDurationCount"]
+    StringToConvertweeklyRetentionDurationCount  = var.rsv_backup_policies[count.index]["weeklyRetentionDurationCount"]
+    StringToConvertmonthlyRetentionDurationCount = var.rsv_backup_policies[count.index]["monthlyRetentionDurationCount"]
   }
-
   #Terraform sends only string to azurerm template, I had to convert it to integer or array into the ARM Template Json, for info : https://github.com/terraform-providers/terraform-provider-azurerm/issues/34
 }
+
