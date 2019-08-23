8de096e64fd1b441d4af9ab0a27af41728afa9a4
James Dumont Le Douarec <j.dumont@veebaze.com>
2019-08-23 18:26:11 +0200

    Update main.tf

--- module/Az-RoleDefinition/main.tf
+++ module/Az-RoleDefinition/main.tf
@@ -1,15 +1,9 @@
 data "azurerm_subscription" "primary" {
 }
 
-resource "random_id" "role_definition_ids" {
-  count       = length(var.roles)
-  byte_length = 16
-}
-
 resource "azurerm_role_definition" "roles" {
   count              = length(var.roles)
   name               = "${var.role_prefix}${var.roles[count.index]["suffix_name"]}${var.role_suffix}"
-  role_definition_id = element(random_id.role_definition_ids.*.hex, count.index)
   scope              = data.azurerm_subscription.primary.id
 
   permissions {
